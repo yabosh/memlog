b2bea1213808d5bd2cf19f32c28c84a80fe49b7f
jshayward <scott.hayward@outlook.com>
2023-11-02 17:14:28 -0500

    Initial commit

---
+++ mem_log.go
@@ -0,0 +1,135 @@
+package memlog
+
+import (
+	"container/list"
+	"sync"
+)
+
+const (
+	allElements = -1
+)
+
+// memLog is a bounded linked list that is intended
+// used as a mechanism for logging information
+// in memory.  The log has a fixed length and
+// supports automatically removing older entries
+// as new entries are added to prevent unbounded growth.
+//
+// memLog is useful in microservice applications where
+// it might be useful to maintain a subset of a log
+// in memory for diagnostics.  For instance the last
+// 1,000 lines of the output log could be saved in memory
+// and made available via a restful API to simplify
+// diagnostics.
+//
+//   - Storing a subset of output logs in memory for
+//     easy access during debugging or diagnostics.
+//
+//   - Storing a short history of results from an operation
+//     that can be reviewed at runtime.
+//
+// memLog is thread-safe
+type memLog[T any] struct {
+	lst    list.List
+	max    int
+	locker sync.Mutex
+}
+
+// NewMemLog returns a new, initialized instance of memlog
+// that will not grow beyond the specified number of
+// entries.  Once the log reaches the maximum number of
+// entries, as new entries are added, the oldest entries
+// are removed.
+func NewMemLog[T any](maxEntries int) *memLog[T] {
+	return &memLog[T]{
+		max: maxEntries,
+	}
+}
+
+// Len returns the number of elements in
+// the log
+func (m *memLog[T]) Len() int {
+	m.locker.Lock()
+	defer m.locker.Unlock()
+	return m.lst.Len()
+}
+
+// Append will add item to the log.  If the
+// log has reached its maximum size the the oldest
+// entry will be removed to make room for the new entry.
+func (m *memLog[T]) Append(item T) {
+	m.locker.Lock()
+	defer m.locker.Unlock()
+
+	m.lst.PushBack(item)
+	if m.lst.Len() > m.max {
+		m.lst.Remove(m.lst.Front())
+	}
+}
+
+// Slice returns the contents of the log as a slice.
+// The slice is ordered from oldest item to the newest
+func (m *memLog[T]) Slice() (slice []T) {
+	return m.SliceN(allElements)
+}
+
+// Clear will clear the current contents of the memLog
+func (m *memLog[T]) Clear() {
+	m.locker.Lock()
+	defer m.locker.Unlock()
+	m.lst.Init()
+}
+
+// SliceN returns the last 'N' items
+// from the log.
+// The slice is ordered from oldest item to the newest
+func (m *memLog[T]) SliceN(n int) (slice []T) {
+	m.locker.Lock()
+	defer m.locker.Unlock()
+
+	len := m.lst.Len()
+
+	if n <= allElements || n > len {
+		n = len
+	}
+
+	return m.toSlice(n)
+}
+
+// toSlice creates a slice of the last 'n' elements
+// of the log.
+func (m *memLog[T]) toSlice(n int) (slice []T) {
+	slice = make([]T, n)
+	idx := n - 1
+
+	// Walk the list 'backward', filling in the slice
+	// from the last element to the zero element.  This
+	// is more efficient than searching 'forward' when n < m.lst.Len()
+	for e := m.lst.Back(); e != nil && idx >= 0; e = e.Prev() {
+		slice[idx] = e.Value.(T)
+		idx--
+	}
+
+	return slice
+}
+
+// toSlice will copy a range of elements in the linked
+// list to a slice
+func (m *memLog[T]) toSlicex(n int, len int) (slice []T) {
+	first := len - n
+	slice = make([]T, n)
+	ptr := 0
+	item := m.lst.Front()
+
+	for i := 0; i < len; i++ {
+		if i < first {
+			item = item.Next()
+			continue
+		}
+		slice[ptr] = item.Value.(T)
+		item = item.Next()
+		ptr++
+	}
+
+	return slice
+}
---
+++ mem_log_test.go
@@ -0,0 +1,182 @@
+package memlog
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+func Test_memlog_get_slice_when_empty(t *testing.T) {
+	// given a memlog
+	log := NewMemLog[string](10)
+
+	// when a slice is created from an empty log
+	slice := log.Slice()
+
+	// then
+	assert.Zero(t, len(slice))
+	assert.Zero(t, log.Len())
+}
+
+func Test_memlog_get_length_when_size_is_less_than_max(t *testing.T) {
+	// given a memlog
+	log := NewMemLog[string](10)
+
+	// when fewer than 'max' entries are added to the log
+	log.Append("item #1")
+
+	// then the length should match the expected number of entries
+	assert.Equal(t, 1, log.Len())
+}
+
+func Test_memlog_get_length_when_log_is_at_max(t *testing.T) {
+	// given a memlog
+	log := NewMemLog[string](2)
+
+	// when more than 'max' entries have been added
+	log.Append("item #1")
+	log.Append("item #2")
+	log.Append("item #3")
+	log.Append("item #4")
+
+	// then the length should match the expected number of entries
+	assert.Equal(t, 2, log.Len())
+	assert.Equal(t, "item #3", log.Slice()[0])
+}
+
+func Test_memlog_get_last_n_entries(t *testing.T) {
+	// given a memlog
+	max := 20
+	log := NewMemLog[string](max)
+
+	// when more than 'max' entries have been added
+	for i := 0; i < max; i++ {
+		log.Append(fmt.Sprintf("item #%d", i))
+	}
+
+	// then the length should match the expected number of entries
+	assert.Equal(t, "item #18", log.SliceN(2)[0])
+}
+
+func Test_memlog_list_memory(t *testing.T) {
+	PrintMemUsage()
+
+	size := 100000
+
+	var l *memLog[string]
+
+	// given a memlog
+	l = NewMemLog[string](size)
+
+	// when an item is appended
+	for i := 0; i < size; i++ {
+		l.Append(fmt.Sprintf("this is a sample log entry that is probaby pretty typical in length %d", i))
+	}
+
+	// then
+	slice := l.Slice()
+	PrintMemUsage()
+	//assert.False(t, true)
+	assert.Equal(t, size, len(slice))
+}
+
+func Test_memlog_list_memory_pointers(t *testing.T) {
+	PrintMemUsage()
+
+	size := 100000
+
+	// given a memlog
+	l := NewMemLog[*string](size)
+
+	// when an item is appended
+	for i := 0; i < size; i++ {
+		msg := fmt.Sprintf("this is a sample log entry that is probaby pretty typical in length %d", i)
+		l.Append(&msg)
+	}
+
+	// then
+	slice := l.Slice()
+	PrintMemUsage()
+	//assert.False(t, true)
+	assert.Equal(t, size, len(slice))
+}
+
+func Benchmark_memlog_list_build_list(b *testing.B) {
+	size := b.N
+
+	// given a memlog
+	l := NewMemLog[string](size)
+
+	// when an item is appended
+	for i := 0; i < b.N; i++ {
+		msg := fmt.Sprintf("this is a sample log entry that is probaby pretty typical in length %d", i)
+		l.Append(msg)
+	}
+}
+
+func Benchmark_memlog_list_build_list_pointers(b *testing.B) {
+	size := b.N
+
+	// given a memlog
+	l := NewMemLog[*string](size)
+
+	// when an item is appended
+	for i := 0; i < b.N; i++ {
+		msg := fmt.Sprintf("this is a sample log entry that is probaby pretty typical in length %d", i)
+		l.Append(&msg)
+	}
+}
+
+func Benchmark_memlog_list_get_slice(b *testing.B) {
+	// given a memlog
+	l := NewMemLog[string](1000)
+
+	for i := 0; i < 1000; i++ {
+		msg := fmt.Sprintf("this is a sample log entry that is probaby pretty typical in length %d", i)
+		l.Append(msg)
+		slice := l.Slice()
+		_ = slice
+	}
+
+	// when an item is appended
+	for i := 0; i < b.N; i++ {
+		slice := l.Slice()
+		_ = slice
+	}
+}
+
+func Benchmark_memlog_list_get_slice_pointers(b *testing.B) {
+	// given a memlog
+	l := NewMemLog[*string](1000)
+
+	for i := 0; i < 1000; i++ {
+		msg := fmt.Sprintf("this is a sample log entry that is probaby pretty typical in length %d", i)
+		l.Append(&msg)
+		slice := l.Slice()
+		_ = slice
+	}
+
+	// when an item is appended
+	for i := 0; i < b.N; i++ {
+		slice := l.Slice()
+		_ = slice
+	}
+}
